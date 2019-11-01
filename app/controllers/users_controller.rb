0faedcde0dc7191422b9911ef3f2380fa460770d
takayuki takahashi <ttkaws15@gmail.com>
2019-11-01 10:47:44 +0900

    ユーザー編集機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -13,7 +15,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "ユーザー情報を編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
@@ -21,5 +33,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :image, :image_cache, :password,
                                  :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   mount_uploader :image, ImageUploader
+  validates :image, presence: true
 end
