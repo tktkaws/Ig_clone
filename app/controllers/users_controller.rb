9ed1845e9b34aa499c63759929a3ec17c9c6e5cb
takayuki takahashi <ttkaws15@gmail.com>
2019-11-01 10:00:43 +0900

    画像投稿機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :image, :password,
+    params.require(:user).permit(:name, :email, :image, :image_cache, :password,
                                  :password_confirmation)
   end
 end
