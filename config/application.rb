2d11914dfa4836b2842d07f33430e020e3fbb1e2
takayuki takahashi <ttkaws15@gmail.com>
2019-11-05 09:06:03 +0900

    Add: Usage: rails generate GENERATOR [args] [options]
    General options:
      -h, [--help]     # Print generator's options and usage
      -p, [--pretend]  # Run but do not make any changes
      -f, [--force]    # Overwrite files that already exist
      -s, [--skip]     # Skip files that already exist
      -q, [--quiet]    # Suppress status output
    
    Please choose a generator below.
    
    Rails:
      application_record
      assets
      channel
      controller
      generator
      helper
      integration_test
      jbuilder
      job
      mailer
      migration
      model
      resource
      scaffold
      scaffold_controller
      system_test
      task
    
    ActiveRecord:
      active_record:application_record
    
    Coffee:
      coffee:assets
    
    Js:
      js:assets
    
    TestUnit:
      test_unit:generator
      test_unit:plugin
    
    Uploader:
      uploaderで不要ファイルを作成しない設定

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,11 @@ module IgClone
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
+
   end
 end
